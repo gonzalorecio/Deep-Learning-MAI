f1827c8c80d42dbaf825c79f629827c5d67a3cd4
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2020-05-04 08:52:03 +0200

    Add Transfer Learning scripts

---
+++ Transfer Learning/launcher.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name="work1-cnn-dropout"
+
+#SBATCH --qos=debug
+
+#SBATCH --workdir=.
+
+#SBATCH --output=work1-cnn_%j.out
+
+#SBATCH --error=work1-cnn_%j.err
+
+#SBATCH --cpus-per-task=40
+
+#SBATCH --gres gpu:1
+
+#SBATCH --time=01:55:00
+
+module purge; module load ffmpeg/4.0.2 gcc/6.4.0 cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 opencv/3.4.1 python/3.6.5_ML
+
+python cnn_fruit_recognition.py
